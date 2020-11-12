2db71d65cd351d1778dfef5353042619b41bb4e4
Crowdbotics <team@crowdbotics.com>
2020-11-12 04:35:48 +0000

    Update models

--- backend/victory_church_22549/wsgi.py
+++ backend/victory_church_22549/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'victory_church_22549.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "victory_church_22549.settings")
 
 application = get_wsgi_application()
